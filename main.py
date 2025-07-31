b473167a7eb2dd015f3663bf60855823cc767f3c
Aliaksei Kudzelka <aleksejkudelka4@gmail.com>
2025-07-31 21:38:56 +0200

    Added last bench functions

--- main.py
+++ main.py
@@ -5,27 +5,47 @@ from threading import Thread
 from typing import Tuple
 from queue import Queue
 
-
+#Top‑level worker: safe to pickle.
+#Calls the right tests.* function and puts its return into results_q.
 def _csv_process_worker(func: str,files: list[str],results_q: Queue) -> None:
-    """
-    Top‑level worker: safe to pickle.
-    Calls the right tests.* function and puts its return into results_q.
-    """
+
     if func == "read":
         tests.read_csvs(files,results_q)
     else:
         tests.write_csvs(files,results_q)
+
+def _query_execution_worker(query:str,time_queue:Queue)->None:
+    tests.query_execution(query,time_queue)
+
+def _copy_data_worker(time_queue:Queue):
+    tests.copy_data(time_queue)
+
+def _tar_files_worker(time_queue:Queue):
+    tests.tar_files(time_queue)
+
+def _gz_files_worker(time_queue:Queue):
+    tests.gz_files(time_queue)
+
+def _fetch_urls_worker(time_queue):
+    tests.fetch_urls(time_queue)
+
+def _loop_sqrt_worker(time_queue):
+    tests.loop_sqrt(time_queue)
+
+def _loop_mult_worker(time_queue):
+    tests.loop_mult(time_queue)
+
+def _loop_pow_worker(time_queue):
+    tests.loop_pow(time_queue)
 
 @tests.timer
 def csv_threading_test(func="read",thread_number:int=2)->tuple[int,int]:
     if func!="read" and func!="write":
         raise ValueError("Function csv_threading_test() cannot have such function argument")
     if func=="read":
-        tests.read_csvs.best = None #type:ignore
-        tests.read_csvs.worst = None #type:ignore
+        tests.read_csvs.times= []
     else:
-        tests.write_csvs.best = None #type:ignore
-        tests.write_csvs.worst = None #type:ignore
+        tests.write_csvs.times= []
     threads:list[Thread]=[]
     success=0
     error=0
@@ -51,11 +71,9 @@ def csv_processing_test(func="read",thread_number:int=2)->tuple[int,int]:
     if func!="read" and func!="write":
         raise ValueError(f"Function csv_processing_test() cannot have such function argument")
     if func=="read":
-        tests.read_csvs.best = None
-        tests.read_csvs.worst = None
+        tests.read_csvs.times= []
     else:
-        tests.write_csvs.best = None
-        tests.write_csvs.worst = None
+        tests.write_csvs.times= []
     processes:list[Process]=[]
     success=0
     error=0
@@ -74,30 +92,533 @@ def csv_processing_test(func="read",thread_number:int=2)->tuple[int,int]:
     return (success,error)
 
 
-def csv_async_test(func="read",thread_number:int=2)->tuple[int,int]:
+@tests.timer
+async def csv_async_test(func="read",thread_number:int=2)->tuple[int,int]:
     if func!="read" and func!="write":
         raise ValueError(f"Function csv_processing_test() cannot have such function argument")
     if func=="read":
-        tests.read_csvs.best = None
-        tests.read_csvs.worst = None
+        tests.read_csvs.times= []
     else:
-        tests.write_csvs.best = None
-        tests.write_csvs.worst = None
+        tests.write_csvs.times= []
     success=0
     error=0
+    tasks=[]
     results_q = Manager().Queue()
-    for i in range(thread_number):
+
+    async def worker(func,results_q):
         if func=="read":
-            asyncio.run(tests.read_csvs_async(tests.TEST_FILES,results_q))
+            await tests.read_csvs_async(tests.TEST_FILES,results_q)
         else:
-            asyncio.run(tests.write_csvs_async(tests.TEST_FILES,results_q))
+            await tests.write_csvs_async(tests.TEST_FILES,results_q)
+
+    for i in range(thread_number):
+        task=asyncio.create_task(worker(func,results_q))
+        tasks.append(task)
+
+    await asyncio.gather(*tasks)
+
     while not results_q.empty():
         s,e=results_q.get()
         success+=s
         error+=e
     return (success,error)
+
+
+@tests.timer
+def query_threading_test(number=2)->tuple[int,int]:
+    tests.query_execution.times= []
+    threads=[]
+    succcess=0
+    error=0
+    time_queue=Queue()
+    for i in range(number):
+        thread=Thread(target=tests.query_execution,
+                      args=(f"SELECT pg_sleep({6})",time_queue))
+        threads.append(thread)
+    for thread in threads:
+        thread.start()
+    for thread in threads:
+        thread.join()
+    while not time_queue.empty():
+        s,e=time_queue.get()
+        succcess+=s
+        error+=e
+    return (succcess,error)
+    
+
+@tests.timer
+def query_processing_test(number=2)->tuple[int,int]:
+    tests.query_execution.times= []
+    processes=[]
+    succcess=0
+    error=0
+    time_queue=Manager().Queue()
+    for i in range(number):
+        process=Process(target=_query_execution_worker,
+                      args=(f"SELECT pg_sleep({6/number})",time_queue))
+        processes.append(process)
+    for process in processes:
+        process.start()
+    for process in processes:
+        process.join()
+    while not time_queue.empty():
+        s,e=time_queue.get()
+        succcess+=s
+        error+=e
+    return (succcess,error)
+
+@tests.timer
+async def query_async_test(number=2)->tuple[int,int]:
+    tests.query_execution.times= []
+    tasks=[]
+    succcess=0
+    error=0
+    time_queue=Manager().Queue()
+    async def worker(query:str,time_queue:Queue):
+        await tests.query_execution_async(f"SELECT pg_sleep({6})",time_queue)
+    for i in range(number):
+        task=asyncio.create_task(worker(f"SELECT pg_sleep({6})",time_queue))
+        tasks.append(task)
+    
+    await asyncio.gather(*tasks)
+    while not time_queue.empty():
+        s,e=time_queue.get()
+        succcess+=s
+        error+=e
+    return (succcess,error)
+
+@tests.timer
+def copy_data_threading_test(number=2)->tuple[int,int]:
+    tests.copy_data.times= []
+    threads=[]
+    succcess=0
+    error=0
+    time_queue=Queue()
+    for i in range(number):
+        thread=Thread(target=tests.copy_data,args=(time_queue,))
+        threads.append(thread)
+    for thread in threads:
+        thread.start()
+    for thread in threads:
+        thread.join()
+    while not time_queue.empty():
+        s,e=time_queue.get()
+        succcess+=s
+        error+=e
+    return (succcess,error)
+
+@tests.timer
+async def copy_data_processing_test(number=2)->tuple[int,int]:
+    tests.copy_data.times= []
+    processes=[]
+    succcess=0
+    error=0
+    time_queue=Manager().Queue()
+    for i in range(number):
+        process=Process(target=_copy_data_worker,args=(time_queue,))
+        processes.append(process)
+    for process in processes:
+        process.start()
+    for process in processes:
+        process.join()
+    while not time_queue.empty():
+        s,e=time_queue.get()
+        succcess+=s
+        error+=e
+    return (succcess,error)
+
+@tests.timer
+async def copy_data_async_test(number=2)->tuple[int,int]:
+    tests.copy_data.times= []
+    tasks=[]
+    succcess=0
+    error=0
+    time_queue=Manager().Queue()
+    async def worker(time_queue):
+        await tests.copy_data_async(time_queue)
+
+    for i in range(number):
+        task=asyncio.create_task(worker(time_queue))
+        tasks.append(task)
+
+    await asyncio.gather(*tasks)
+    while not time_queue.empty():
+        s,e=time_queue.get()
+        succcess+=s
+        error+=e
+    return (succcess,error)
+
+@tests.timer
+def tar_files_threading_test(number=2)->tuple[int,int]:
+    tests.tar_files.times= []
+    threads=[]
+    succcess=0
+    error=0
+    time_queue=Queue()
+    for i in range(number):
+        thread=Thread(target=tests.tar_files,args=(time_queue,))
+        threads.append(thread)
+    for thread in threads:
+        thread.start()
+    for thread in threads:
+        thread.join()
+    while not time_queue.empty():
+        s,e=time_queue.get()
+        succcess+=s
+        error+=e
+    return (succcess,error)
+
+@tests.timer
+def tar_files_processing_test(number=2)->tuple[int,int]:
+    tests.tar_files.times= []
+    processes=[]
+    succcess=0
+    error=0
+    time_queue=Manager().Queue()
+    for i in range(number):
+        process=Process(target=_tar_files_worker,args=(time_queue,))
+        processes.append(process)
+    for process in processes:
+        process.start()
+    for process in processes:
+        process.join()
+    while not time_queue.empty():
+        s,e=time_queue.get()
+        succcess+=s
+        error+=e
+    return (succcess,error)
+
+@tests.timer
+async def tar_files_async_test(number=2)->tuple[int,int]:
+    tests.tar_files.times= []
+    tasks=[]
+    succcess=0
+    error=0
+    time_queue=Manager().Queue()
+    async def worker():
+        await tests.tar_files_async(time_queue)
+
+    for i in range(number):
+        task=asyncio.create_task(worker())
+        tasks.append(task)
+
+    await asyncio.gather(*tasks)
+    while not time_queue.empty():
+        s,e=time_queue.get()
+        succcess+=s
+        error+=e
+    return (succcess,error)
+
+@tests.timer
+def gz_files_threading_test(number=2)->tuple[int,int]:
+    tests.gz_files.times= []
+    threads=[]
+    succcess=0
+    error=0
+    time_queue=Queue()
+    for i in range(number):
+        thread=Thread(target=tests.gz_files,args=(time_queue,))
+        threads.append(thread)
+    for thread in threads:
+        thread.start()
+    for thread in threads:
+        thread.join()
+    while not time_queue.empty():
+        s,e=time_queue.get()
+        succcess+=s
+        error+=e
+    return (succcess,error)
+
+@tests.timer
+def gz_files_processing_test(number=2)->tuple[int,int]:
+    tests.gz_files.times= []
+    processes=[]
+    succcess=0
+    error=0
+    time_queue=Manager().Queue()
+    for i in range(number):
+        process=Process(target=_gz_files_worker,args=(time_queue,))
+        processes.append(process)
+    for process in processes:
+        process.start()
+    for process in processes:
+        process.join()
+    while not time_queue.empty():
+        s,e=time_queue.get()
+        succcess+=s
+        error+=e
+    return (succcess,error)
+
+@tests.timer
+async def gz_files_async_test(number=2)->tuple[int,int]:
+    tests.tar_files.times= []
+    tasks=[]
+    succcess=0
+    error=0
+    time_queue=Manager().Queue()
+    async def worker():
+        await tests.gz_files_async(time_queue)
+
+    for i in range(number):
+        task=asyncio.create_task(worker())
+        tasks.append(task)
+
+    await asyncio.gather(*tasks)
+    while not time_queue.empty():
+        s,e=time_queue.get()
+        succcess+=s
+        error+=e
+    return (succcess,error)
+
+@tests.timer
+def fetch_urls_threading_test(number=2)->tuple[int,int]:
+    tests.fetch_urls.times= []
+    threads=[]
+    succcess=0
+    error=0
+    time_queue=Queue()
+    for i in range(number):
+        thread=Thread(target=tests.fetch_urls,args=(time_queue,))
+        threads.append(thread)
+    for thread in threads:
+        thread.start()
+    for thread in threads:
+        thread.join()
+    while not time_queue.empty():
+        s,e=time_queue.get()
+        succcess+=s
+        error+=e
+    return (succcess,error)
+
+@tests.timer
+def fetch_urls_processing_test(number=2)->tuple[int,int]:
+    tests.fetch_urls.times= []
+    processes=[]
+    succcess=0
+    error=0
+    time_queue=Manager().Queue()
+    for i in range(number):
+        process=Process(target=_fetch_urls_worker,args=(time_queue,))
+        processes.append(process)
+    for process in processes:
+        process.start()
+    for process in processes:
+        process.join()
+    while not time_queue.empty():
+        s,e=time_queue.get()
+        succcess+=s
+        error+=e
+    return (succcess,error)
+
+@tests.timer
+async def fetch_urls_async_test(number=2)->tuple[int,int]:
+    tests.fetch_urls.times= []
+    tasks=[]
+    succcess=0
+    error=0
+    time_queue=Manager().Queue()
+    async def worker():
+        await tests.fetch_urls_async(time_queue)
+
+    for i in range(number):
+        task=asyncio.create_task(worker())
+        tasks.append(task)
+
+    await asyncio.gather(*tasks)
+    while not time_queue.empty():
+        s,e=time_queue.get()
+        succcess+=s
+        error+=e
+    return (succcess,error)
+
+@tests.timer
+def loop_sqrt_threading_test(number=2)->tuple[int,int]:
+    tests.loop_sqrt.times= []
+    threads=[]
+    succcess=0
+    error=0
+    time_queue=Queue()
+    for i in range(number):
+        thread=Thread(target=tests.loop_sqrt,args=(time_queue,))
+        threads.append(thread)
+    for thread in threads:
+        thread.start()
+    for thread in threads:
+        thread.join()
+    while not time_queue.empty():
+        s,e=time_queue.get()
+        succcess+=s
+        error+=e
+    return (succcess,error)
+
+@tests.timer
+def loop_sqrt_processing_test(number=2)->tuple[int,int]:
+    tests.loop_sqrt.times= []
+    processes=[]
+    succcess=0
+    error=0
+    time_queue=Manager().Queue()
+    for i in range(number):
+        process=Process(target=_loop_sqrt_worker,args=(time_queue,))
+        processes.append(process)
+    for process in processes:
+        process.start()
+    for process in processes:
+        process.join()
+    while not time_queue.empty():
+        s,e=time_queue.get()
+        succcess+=s
+        error+=e
+    return (succcess,error)
+
+@tests.timer
+async def loop_sqrt_async_test(number=2)->tuple[int,int]:
+    tests.loop_sqrt.times= []
+    tasks=[]
+    succcess=0
+    error=0
+    time_queue=Manager().Queue()
+    async def worker():
+        await tests.loop_sqrt_async(time_queue)
+
+    for i in range(number):
+        task=asyncio.create_task(worker())
+        tasks.append(task)
+
+    await asyncio.gather(*tasks)
+    while not time_queue.empty():
+        s,e=time_queue.get()
+        succcess+=s
+        error+=e
+    return (succcess,error)
+
+@tests.timer
+def loop_pow_threading_test(number=2)->tuple[int,int]:
+    tests.loop_pow.times= []
+    threads=[]
+    succcess=0
+    error=0
+    time_queue=Queue()
+    for i in range(number):
+        thread=Thread(target=tests.loop_pow,args=(time_queue,))
+        threads.append(thread)
+    for thread in threads:
+        thread.start()
+    for thread in threads:
+        thread.join()
+    while not time_queue.empty():
+        s,e=time_queue.get()
+        succcess+=s
+        error+=e
+    return (succcess,error)
+
+@tests.timer
+def loop_pow_processing_test(number=2)->tuple[int,int]:
+    tests.loop_pow.times= []
+    processes=[]
+    succcess=0
+    error=0
+    time_queue=Manager().Queue()
+    for i in range(number):
+        process=Process(target=_loop_pow_worker,args=(time_queue,))
+        processes.append(process)
+    for process in processes:
+        process.start()
+    for process in processes:
+        process.join()
+    while not time_queue.empty():
+        s,e=time_queue.get()
+        succcess+=s
+        error+=e
+    return (succcess,error)
+
+@tests.timer
+async def loop_pow_async_test(number=2)->tuple[int,int]:
+    tests.loop_pow.times= []
+    tasks=[]
+    succcess=0
+    error=0
+    time_queue=Manager().Queue()
+    async def worker():
+        await tests.loop_pow_async(time_queue)
+
+    for i in range(number):
+        task=asyncio.create_task(worker())
+        tasks.append(task)
+
+    await asyncio.gather(*tasks)
+    while not time_queue.empty():
+        s,e=time_queue.get()
+        succcess+=s
+        error+=e
+    return (succcess,error)
+
+@tests.timer
+def loop_mult_threading_test(number=2)->tuple[int,int]:
+    tests.loop_mult.times= []
+    threads=[]
+    succcess=0
+    error=0
+    time_queue=Queue()
+    for i in range(number):
+        thread=Thread(target=tests.loop_mult,args=(time_queue,))
+        threads.append(thread)
+    for thread in threads:
+        thread.start()
+    for thread in threads:
+        thread.join()
+    while not time_queue.empty():
+        s,e=time_queue.get()
+        succcess+=s
+        error+=e
+    return (succcess,error)
+
+@tests.timer
+def loop_mult_processing_test(number=2)->tuple[int,int]:
+    tests.loop_mult.times= []
+    processes=[]
+    succcess=0
+    error=0
+    time_queue=Manager().Queue()
+    for i in range(number):
+        process=Process(target=_loop_mult_worker,args=(time_queue,))
+        processes.append(process)
+    for process in processes:
+        process.start()
+    for process in processes:
+        process.join()
+    while not time_queue.empty():
+        s,e=time_queue.get()
+        succcess+=s
+        error+=e
+    return (succcess,error)
+
+@tests.timer
+async def loop_mult_async_test(number=2)->tuple[int,int]:
+    tests.loop_mult.times= []
+    tasks=[]
+    succcess=0
+    error=0
+    time_queue=Manager().Queue()
+    async def worker():
+        await tests.loop_mult_async(time_queue)
+
+    for i in range(number):
+        task=asyncio.create_task(worker())
+        tasks.append(task)
+
+    await asyncio.gather(*tasks)
+    while not time_queue.empty():
+        s,e=time_queue.get()
+        succcess+=s
+        error+=e
+    return (succcess,error)
+
+
 if __name__ == "__main__":
-    print(csv_async_test("read",4))
-    print(tests.read_csvs.times)
-    print(csv_async_test("write",4))
-    print(tests.write_csvs.times)
+    print(loop_mult_threading_test())
+    print(loop_mult_threading_test.times)
+    print(tests.loop_mult.times)
+    # print(asyncio.run(loop_mult_async_test()))
+    # print(loop_mult_async_test.times)
+    # print(tests.loop_mult.times)
--- scripts/tests.py
+++ scripts/tests.py
@@ -4,6 +4,7 @@ from queue import Queue
 import pandas as pd
 from scripts.setup_db import server_connect,server_disconnect,reset_db
 from psycopg2.extensions import connection as Connection
+from inspect import iscoroutinefunction as is_async
 
 CONST=12000
 TEST_FILES=["customers-100000","leads-100000","organizations-100000","products-100000"]
@@ -12,14 +13,24 @@ URLS=["https://randomuser.me/api/","https://dog.ceo/api/breeds/image/random","ht
 
 
 def timer(func):
-    def wrapper(*args,**kwargs):
-        start=time.perf_counter()
-        result=func(*args,**kwargs)
-        end=time.perf_counter()
-        wrapper.times.append(end-start)
-        return result
-    wrapper.times=[] #type:ignore
-    return wrapper
+    if not is_async(func):
+        def sync_wrapper(*args,**kwargs):
+            start=time.perf_counter()
+            result=func(*args,**kwargs)
+            end=time.perf_counter()
+            sync_wrapper.times.append(end-start)
+            return result
+        sync_wrapper.times=[] #type:ignore
+        return sync_wrapper
+    else:
+        async def async_wrapper(*args,**kwargs):
+            start=time.perf_counter()
+            result=await func(*args,**kwargs)
+            end=time.perf_counter()
+            async_wrapper.times.append(end-start)
+            return result
+        async_wrapper.times=[] #type:ignore
+        return async_wrapper
 
 
 def clear_dataset()->None:
@@ -67,75 +78,136 @@ async def write_csvs_async(files:List[str],results_q:Queue)->None:
     await loop.run_in_executor(None,write_csvs,files,results_q)
 
 @timer
-def query_execution(query:str)->None:
+def query_execution(query:str,time_queue:Queue)->None:
     connection:Connection|None=server_connect()
-    if connection:
-        with connection.cursor() as cursor:
-            cursor.execute(query)
-    server_disconnect(connection)
+    try:
+        if connection:
+            with connection.cursor() as cursor:
+                cursor.execute(query)
+        time_queue.put((1,0))
+    except:
+        time_queue.put((0,1))
+    finally:
+        server_disconnect(connection)
 
-async def query_execution_async(query:str)->None:
+async def query_execution_async(query:str,time_queue:Queue)->None:
     loop=asyncio.get_running_loop()
-    await loop.run_in_executor(None,query_execution,query)
+    await loop.run_in_executor(None,query_execution,query,time_queue)
 
 @timer
-def fetch_urls(number:int=5):
+def fetch_urls(time_queue:Queue,number:int=5):
+    success=0
+    error=0
     for i in range(number):
         for url in URLS:
-            resp = requests.get(url)
+            try:
+                resp = requests.get(url)
+                success+=1
+            except:
+                error+=1
+    time_queue.put((success,error))
 
-async def fetch_urls_async(number:int=1):
+async def fetch_urls_async(time_queue:Queue,number:int=5):
     loop=asyncio.get_running_loop()
-    await loop.run_in_executor(None,fetch_urls,number)
+    await loop.run_in_executor(None,fetch_urls,time_queue,number)
 
-#########################################
+########################################
 ############# OS-BOUND TESTS ###########
-#########################################
+########################################
 
 @timer
-def copy_data(count:int=2000000):
-    os.system(f"dd if=/dev/urandom of=test.txt count={count} bs=1")
-    os.system("rm -f text.txt")
-    clear_dataset()
+def copy_data(time_queue:Queue,count:int=1000000):
+    try:
+        os.system(f"dd if=/dev/urandom of=test.txt count={count} bs=1")
+        os.system("rm -f text.txt")
+        clear_dataset()
+        time_queue.put((1,0))
+    except:
+        time_queue.put((0,1))
 
-async def copy_data_async(count:int=2000000):
+async def copy_data_async(time_queue:Queue,count:int=2000000):
     loop=asyncio.get_running_loop()
-    await loop.run_in_executor(None,copy_data,count)
+    await loop.run_in_executor(None,copy_data,time_queue,count)
 
 @timer
-def tar_files(number:int=1):
+def tar_files(time_queue:Queue,number:int=1):
     for i in range(number):
-        os.system("""tar -czf datasets/archive.tar.gz datasets/* &&
-                   rm datasets/*.csv && tar -xzf datasets/archive.tar.gz &&
-                  rm datasets/*.gz""")
+        try:
+            os.system("""tar -czf datasets/archive.tar.gz datasets/* &&
+                    rm datasets/*.csv && tar -xzf datasets/archive.tar.gz &&
+                    rm datasets/*.gz""")
+            time_queue.put((1,0))
+        except:
+            time_queue.put((0,1))
+
     
-async def tar_files_async(number:int=1):
+async def tar_files_async(time_queue:Queue,number:int=1):
     loop=asyncio.get_running_loop()
-    await loop.run_in_executor(None,tar_files,number)
+    await loop.run_in_executor(None,tar_files,time_queue,number)
 
 @timer
-def gz_files(number:int=1):
+def gz_files(time_queue:Queue,number:int=1):
     for i in range(number):
-        os.system("gzip datasets/*")
-        os.system("gunzip datasets/*")
+        try:
+            os.system("gzip -f datasets/*")
+            os.system("gunzip datasets/*")
+            time_queue.put((1,0))
+        except:
+            time_queue.put((0,1))
 
-async def gz_files_async(number:int=1):
+async def gz_files_async(time_queue:Queue,number:int=1):
     loop=asyncio.get_running_loop()
-    await loop.run_in_executor(None,gz_files,number)
+    await loop.run_in_executor(None,gz_files,time_queue,number)
 
 #########################################
 ############# CPU-BOUND TESTS ###########
 #########################################
 
 @timer
-def loop_sqrt():
-    return [math.sqrt(itr) for itr in range(CONST**2)]
+def loop_sqrt(time_queue:Queue)->None:
+    success=0
+    error=0
+    try:
+        for itr in range(CONST**2):
+            _=math.sqrt(itr) 
+        success+=1
+    except:
+        error+=1
+    return time_queue.put((success,error))
+
+async def loop_sqrt_async(time_queue:Queue)->None:
+    loop=asyncio.get_running_loop()
+    await loop.run_in_executor(None,loop_sqrt,time_queue)
 
 @timer
-def loop_pow():
-    return [math.pow(itr,itr) for itr in range(CONST)]
+def loop_pow(time_queue:Queue):
+    success=0
+    error=0
+    try:
+        for itr in range(CONST):
+            _=itr**itr
+        success+=1
+    except:
+        error+=1
+    return time_queue.put((success,error))
+
+async def loop_pow_async(time_queue:Queue)->None:
+    loop=asyncio.get_running_loop()
+    await loop.run_in_executor(None,loop_pow,time_queue)
 
 @timer
-def loop_mult():
-    return [itr*itr for itr in range(CONST**2)]
+def loop_mult(time_queue:Queue):
+    success=0
+    error=0
+    try:
+        for itr in range(CONST**2):
+            _=itr*itr
+        success+=1
+    except:
+        error+=1
+    return time_queue.put((success,error))
+
+async def loop_mult_async(time_queue:Queue)->None:
+    loop=asyncio.get_running_loop()
+    await loop.run_in_executor(None,loop_mult,time_queue)
 
